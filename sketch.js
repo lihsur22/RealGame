28874c06082937e3e847f140bbfa3a774320d9d7
lihsur22 <rushilganguli@outlook.com>
2020-10-03 15:18:18 +0530

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,30 +1,11 @@
-var walls, w1, w2, w3, w4, w5, w;
-var player, playC, allPlayer;
-var breaks, b1, b2, b3, b4;
-var db, gameState = 0;
-var form, game;
+var player, fightObj, itemObj;
+var statsP = [hp, def, dext, critChance];
 
 function setup(){
-  canvas = createCanvas(displayWidth-20, displayHeight-20);
-
-  db = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
+  canvas = createCanvas(800,800);
+  
 }
 
 function draw(){
-  if(playC == 4 && gameState !== 2)
-  {
-    game.update(1);
-  }
-  if(gameState == 1)
-  {
-    clear();
-    game.play();
-  }
-  if(gameState ==2)
-  {
-    game.end();
-  }
+  background(0);
 }
